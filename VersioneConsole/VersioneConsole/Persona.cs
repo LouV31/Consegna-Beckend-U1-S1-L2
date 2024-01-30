5b44639194202742fe5cb768347bc1924b6e4425
LouV31 <luigi.valentino99@yahoo.it>
2024-01-30 15:01:03 +0100

    Aggiungere i file di progetto.

---
+++ VersioneConsole/VersioneConsole/Persona.cs
@@ -0,0 +1,40 @@
+﻿namespace VersioneConsole
+{
+    internal class Persona
+    {
+
+        private string _nome;
+        private string _cognome;
+        private int _età;
+
+        public Persona(string nome, string cognome, int età)
+        {
+            this._nome = nome;
+            this._cognome = cognome;
+            this._età = età;
+        }
+
+        public string GetNome()
+        {
+            return this._nome;
+        }
+
+        public string GetCognome()
+        {
+            return this._cognome;
+
+        }
+
+        public int GetEtà()
+        {
+            return this._età;
+        }
+
+        public string GetDettagli()
+        {
+
+            return this._nome + " " + this._cognome + " " + this._età;
+        }
+    }
+
+}
---
+++ VersioneConsole/VersioneConsole/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace VersioneConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Inserisci il nome: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("\n");
+            Console.WriteLine("Inserisci il cognome: ");
+            string cognome = Console.ReadLine();
+            Console.WriteLine("\n");
+            Console.WriteLine("Inserisci l'età");
+            int età;
+            while (!int.TryParse(Console.ReadLine(), out età))
+            {
+                Console.WriteLine("Devi inserire un numero.");
+            }
+            Console.WriteLine("\n");
+
+            Persona persona1 = new Persona(nome, cognome, età);
+            Console.WriteLine("Solo il nome: ");
+            Console.WriteLine(persona1.GetNome());
+            Console.WriteLine("\n");
+            Console.WriteLine("Solo il Cognome: ");
+            Console.WriteLine(persona1.GetCognome());
+            Console.WriteLine("\n");
+            Console.WriteLine("Solo l'età: ");
+            Console.WriteLine(persona1.GetEtà());
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Profilo persona completo");
+            Console.WriteLine(persona1.GetDettagli());
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
